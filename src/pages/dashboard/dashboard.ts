ca2cf783227585aba77a79c962534be21b379cef
Estrada Matthieu <ttamalfor@gmail.com>
2018-05-10 22:10:00 +0200

    Make dahsboard functional, clean files

--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
 import { BackendClient } from "../../backend/client.service";
 
 @IonicPage()
@@ -9,21 +9,71 @@ import { BackendClient } from "../../backend/client.service";
   providers: [BackendClient]
 })
 export class Dashboard {
-  public data = {hosts: {}, services: {}, nb_items: {host: 0, service: 0, total: 0}};
-  public livesynthesis = {
+  private livesynthesis = {
     monitored: {host: 0, service: 0, total: 0},
     problems: {host: 0, service: 0, total: 0},
   };
+  private icon_colors = {
+    host: 'secondary',
+    service: 'secondary',
+    total: 'secondary',
+  };
 
-  constructor(public navCtrl: NavController, public client: BackendClient) {
+  constructor(public client: BackendClient) {
     this.client.get_livesynthesis().subscribe(
       function (data) {
-        this.livesynthesis.monitored.host = data['_items'][0]['hosts_total'];
-        this.livesynthesis.monitored.service = data['_items'][0]['services_total'];
-        this.livesynthesis.monitored.total = data['_items'][0]['hosts_total'] + data['_items'][0]['services_total'];
+        this.manageData(data);
         }.bind(this),
           err => console.log('Synth err', err)
     )
+  }
+
+  protected manageData(data: {}){
+    let livesynth = data['_items'][0];
+    // Define totals
+    this.livesynthesis.monitored.host = livesynth['hosts_total'];
+    this.livesynthesis.monitored.service = livesynth['services_total'];
+    this.livesynthesis.monitored.total = livesynth['hosts_total'] + livesynth['services_total'];
+
+    // Define problems
+    let hosts_problem_keys = ['hosts_down_hard', 'hosts_down_soft', 'hosts_unreachable_hard', 'hosts_unreachable_soft'];
+    livesynth[hosts_problem_keys[2]] += 1;
+    for (let hostKey in hosts_problem_keys) {
+      if (!Object.prototype.hasOwnProperty.call(livesynth, hostKey)) {
+        this.livesynthesis.problems.host += livesynth[hosts_problem_keys[hostKey]];
+      }
+    }
+
+    let services_problem_keys = ['services_critical_hard', 'services_critical_soft','services_warning_hard',
+      'services_warning_soft', 'services_unreachable_hard', 'services_unreachable_soft'];
+    for (let serviceKey in services_problem_keys) {
+      if (!Object.prototype.hasOwnProperty.call(livesynth, serviceKey)) {
+        this.livesynthesis.problems.service += livesynth[services_problem_keys[serviceKey]];
+      }
+    }
+
+    this.livesynthesis.problems.total = this.livesynthesis.problems.host + this.livesynthesis.problems.service;
+    this.update_colors()
+  }
+
+  private update_colors(){
+    if (this.livesynthesis.problems.host > 0) {
+      this.icon_colors.host = 'danger'
+    } else {
+      this.icon_colors.host = 'secondary'
+    }
+
+    if (this.livesynthesis.problems.service > 0) {
+      this.icon_colors.service = 'danger'
+    } else {
+      this.icon_colors.service = 'secondary'
+    }
+
+    if (this.livesynthesis.problems.total> 0) {
+      this.icon_colors.total = 'danger'
+    } else {
+      this.icon_colors.total = 'secondary'
+    }
   }
 
 }

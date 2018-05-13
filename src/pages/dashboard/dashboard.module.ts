11a8663a666938a63ddcd14a1d943ab2a3fcd632
Estrada Matthieu <ttamalfor@gmail.com>
2018-05-13 09:52:40 +0200

    Add ng-circles for dashboard total, hosts, services

--- src/pages/dashboard/dashboard.module.ts
+++ src/pages/dashboard/dashboard.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { Dashboard} from "./dashboard";
+import {NgCircleProgressModule} from "ng-circle-progress";
 
 @NgModule({
   declarations: [
@@ -8,6 +9,14 @@ import { Dashboard} from "./dashboard";
   ],
   imports: [
     IonicPageModule.forChild(Dashboard),
+    NgCircleProgressModule.forRoot({
+      radius: 100,
+      outerStrokeWidth: 16,
+      innerStrokeWidth: 8,
+      outerStrokeColor: "#78C000",
+      innerStrokeColor: "#C7E596",
+      animationDuration: 300
+    })
   ],
 })
 export class DashboardModule {}
--- src/pages/dashboard/dashboard.ts
+++ src/pages/dashboard/dashboard.ts
@@ -3,6 +3,11 @@ import { IonicPage } from 'ionic-angular';
 
 import { BackendClient } from "../../backend/client.service";
 
+
+const OK   = '#27ae60';
+const WARN = '#e67e22';
+const CRITICAL = '#e74c3c';
+
 @IonicPage()
 @Component({
   selector: 'page-dashboard',
@@ -10,14 +15,15 @@ import { BackendClient } from "../../backend/client.service";
   providers: [BackendClient]
 })
 export class Dashboard {
-  private livesynthesis = {
+  private livestate = {
     monitored: {host: 0, service: 0, total: 0},
     problems: {host: 0, service: 0, total: 0},
+    percent: {host: '0.0', service: '0.0', total: '0.0'}
   };
   private colors = {
-    host: 'ok',
-    service: 'ok',
-    total: 'total',
+    host: {outerStrokeColor: OK, innerStrokeColor: OK},
+    service: {outerStrokeColor: OK, innerStrokeColor: OK},
+    total: {outerStrokeColor: OK, innerStrokeColor: OK},
   };
 
   constructor(public client: BackendClient) {
@@ -35,16 +41,16 @@ export class Dashboard {
       let livesynth = data['_items'][i];
 
       // Define totals
-      this.livesynthesis.monitored.host += livesynth['hosts_total'];
-      this.livesynthesis.monitored.service += livesynth['services_total'];
-      this.livesynthesis.monitored.total += livesynth['hosts_total'] + livesynth['services_total'];
+      this.livestate.monitored.host += livesynth['hosts_total'];
+      this.livestate.monitored.service += livesynth['services_total'];
+      this.livestate.monitored.total += livesynth['hosts_total'] + livesynth['services_total'];
 
       // Define problems
       let hosts_problem_keys = [
         'hosts_down_hard', 'hosts_down_soft', 'hosts_unreachable_hard', 'hosts_unreachable_soft'];
       for (let hostKey in hosts_problem_keys) {
         if (!Object.prototype.hasOwnProperty.call(livesynth, hostKey)) {
-          this.livesynthesis.problems.host += livesynth[hosts_problem_keys[hostKey]];
+          this.livestate.problems.host += livesynth[hosts_problem_keys[hostKey]];
         }
       }
 
@@ -53,33 +59,61 @@ export class Dashboard {
         'services_warning_soft', 'services_unreachable_hard', 'services_unreachable_soft'];
       for (let serviceKey in services_problem_keys) {
         if (!Object.prototype.hasOwnProperty.call(livesynth, serviceKey)) {
-          this.livesynthesis.problems.service += livesynth[services_problem_keys[serviceKey]];
+          this.livestate.problems.service += livesynth[services_problem_keys[serviceKey]];
         }
+      }
+      this.livestate.problems.total = this.livestate.problems.host + this.livestate.problems.service;
+    }
+
+    // Update display
+    this.updateDashboard();
+  }
+
+  private updateDashboard() {
+    this.livestate.percent.host =
+      (100 - (this.livestate.problems.host / this.livestate.monitored.host) * 100).toFixed(2);
+    this.livestate.percent.service =
+      (100 - (this.livestate.problems.service / this.livestate.monitored.service) * 100).toFixed(2);
+    this.livestate.percent.total =
+      ((this.livestate.problems.total / this.livestate.monitored.total) * 100).toFixed(2);
+
+    this.updateColors()
+  }
+
+  private updateColors() {
+    // Total colors
+    if(+this.livestate.percent.total != 0) {
+      this.colors.total.outerStrokeColor = CRITICAL
+    } else {
+      this.colors.total.outerStrokeColor = OK
+    }
+
+    if (+this.livestate.percent.total == 0) {
+      this.colors.total.innerStrokeColor = OK
+    } else if (+this.livestate.percent.total < 50) {
+      this.colors.total.innerStrokeColor = WARN
+    } else {
+      this.colors.total.innerStrokeColor = CRITICAL
+    }
+
+    // Host / Service colors
+    function defineColor(percent, color){
+      if (percent == 100) {
+        color.outerStrokeColor = OK
+      } else {
+        color.outerStrokeColor = CRITICAL
+      }
+      if (percent == 100) {
+        color.innerStrokeColor = OK
+      } else if (percent >= 50) {
+        color.innerStrokeColor = WARN
+      } else {
+        color.innerStrokeColor = CRITICAL
       }
     }
 
-    this.livesynthesis.problems.total = this.livesynthesis.problems.host + this.livesynthesis.problems.service;
-    this.update_colors()
-  }
-
-  private update_colors(){
-    if (this.livesynthesis.problems.host > 0) {
-      this.colors.host = 'ko'
-    } else {
-      this.colors.host = 'ok'
-    }
-
-    if (this.livesynthesis.problems.service > 0) {
-      this.colors.service = 'ko'
-    } else {
-      this.colors.service = 'ok'
-    }
-
-    if (this.livesynthesis.problems.total > 0) {
-      this.colors.total = 'ko'
-    } else {
-      this.colors.total = 'total'
-    }
+    defineColor(+this.livestate.percent.host, this.colors.host);
+    defineColor(+this.livestate.percent.service, this.colors.service);
   }
 
 }

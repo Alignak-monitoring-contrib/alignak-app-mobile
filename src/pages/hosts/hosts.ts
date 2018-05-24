0472d58a01d628e2771fb29e32f9ca0ae4552ba8
Matthieu Estrada <ttamalfor@gmail.com>
2018-05-24 16:02:04 +0200

    Tests for filtering objects

--- src/pages/hosts/hosts.ts
+++ src/pages/hosts/hosts.ts
@@ -14,6 +14,7 @@ export class HostsPage {
   public hosts = [];
   public colors = {up: 'up', unreachable: 'unreachable', down: 'down' };
   public nextPage = 'host';
+  public criteria = '';
 
   constructor(public navCtrl: NavController, public client: BackendClient) {
     this.addHosts()
@@ -31,7 +32,32 @@ export class HostsPage {
 
     )
   }
+
+  public compare = (d, b)=>  {
+    let field = 'ls_state';
+    if (this.criteria.includes(':'))
+      field = this.criteria.split(':', 2)[0];
+    let criteria = this.criteria.split(':', 2)[1];
+    console.log('Field ', field);
+    console.log('Criter ', criteria);
+
+    if (d[field] != criteria || !d[field].includes(criteria))
+      return 1;
+    if (d[field] == criteria || d[field].includes(criteria))
+      return -1;
+    return 0;
+  };
+
+  eventHandler(keyCode) {
+    // Catch when user type ENTER in "this.password"
+    // if (keyCode === 13){
+    //   this.hosts = this.hosts.sort(this.compare)
+    // }
+    this.hosts = this.hosts.sort(this.compare)
+  }
+
   public displayHost(host): void {
+    // Push to HostSynthesis Page
     this.navCtrl.push(HostSynthesisPage, {host: host})
   }
 

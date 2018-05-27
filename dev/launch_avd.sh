eef30a818a1e5cccc37253a2a516f1abad666409
Estrada Matthieu <ttamalfor@gmail.com>
2018-05-27 23:27:26 +0200

    Remove dev folder, clean travis file
    Ref #12

--- dev/launch_avd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Launching Nexus 5"
-
-CUR_SDK=$HOME/Android/Sdk
-export LD_LIBRARY_PATH=$CUR_SDK/emulator/lib64/qt/lib:$CUR_SDK/emulator/lib64/
-
-$CUR_SDK/emulator/qemu/linux-x86_64/qemu-system-x86_64 -netdelay none -netspeed full -avd AVD_for_Nexus_6_by_Google

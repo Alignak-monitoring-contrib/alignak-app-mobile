12c460d9187d1754951a555a8d7d96f45eaf596f
Estrada Matthieu <ttamalfor@gmail.com>
2018-05-27 22:56:24 +0200

    Try again with export DISPLAY
    Ref #12

--- test-config/karma.conf.js
+++ test-config/karma.conf.js
@@ -58,9 +58,9 @@ module.exports = function(config) {
     singleRun: false
   };
 
-  if (process.env.TRAVIS) {
-    _config.browsers = ['Chrome_travis_ci'];
-  }
+  // if (process.env.TRAVIS) {
+  //   _config.browsers = ['Chrome_travis_ci'];
+  // }
 
   config.set(_config);
 };
